a3c7a79f48e99f191f95cddbc0147d4b40be681f
yichuang-zh <44933638+yichuang-zh@users.noreply.github.com>
2024-08-19 20:01:01 +0800

    2024-08-19 升级

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,13 +1,12 @@
-const DOCUMENTATION_OPTIONS = {
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
     VERSION: '',
-    LANGUAGE: 'zh-CN',
+    LANGUAGE: 'zh_CN',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
     LINK_SUFFIX: '.html',
     HAS_SOURCE: false,
     SOURCELINK_SUFFIX: '.txt',
-    NAVIGATION_WITH_KEYS: false,
-    SHOW_SEARCH_SUMMARY: true,
-    ENABLE_SEARCH_SHORTCUTS: true,
+    NAVIGATION_WITH_KEYS: false
 };
