804fd2e8e321b75b00f14cb25cb5c4c6ae6521ba
yichuang-zh <44933638+yichuang-zh@users.noreply.github.com>
2025-07-14 16:47:36 +0800

    for new domain

--- middleware.js
+++
@@ -1,9 +0,0 @@
-import { NextResponse } from 'next/server';
-
-export function middleware(request) {
-  const host = request.headers.get('host');
-  if (host === 'ycproject.cn' && request.nextUrl.pathname === '/') {
-    return NextResponse.rewrite(new URL('/old.html', request.url));
-  }
-  return NextResponse.next();
-} 
